e7e803a2fd0489301f892bb2c38f4cdf8a52267c
Dawit-coder <dawit3945@gmail.com>
2025-07-31 09:15:47 +0300

    add userCredits function to userController

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -44,12 +44,24 @@ const loginUser = async (req, res) =>{
             const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
             res.json({success: true, token, user:{name: user.name}})
         }else{
-            return res({success: false, message: 'Invalid credentials' })
+            return res.json({success: false, message: 'Invalid credentials' })
         }
 
     } catch (error) {
         console.log(error)
         res.json({success: false, message: error.message})
+    }
+}
+
+const userCredits = async (req, res) =>{
+    try {
+        const {userId} = req.body
+
+        const user = await userModel.findById(userId)
+        res.json({success: true, credits:user.creditBalance, user: {name: user.name}})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false, message: error.message})
     }
 }
 
